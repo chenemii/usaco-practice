4deb00da097b26c5bb30abe5a1d6f547a7ba7e45
Emily Chen <119235199+chenemii@users.noreply.github.com>
2023-10-12 21:38:54 -0400

    Create lostcow.java

---
+++ lostcow.java
@@ -0,0 +1,41 @@
+// Source: https://usaco.guide/general/io
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException 
+	{
+		BufferedReader br = new BufferedReader(new FileReader("lostcow.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("lostcow.out")));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int john = Integer.parseInt(st.nextToken());
+		int cow = Integer.parseInt(st.nextToken());
+
+		int totaldistance = 0;
+		int disttravel = 1;
+		int direction = 1;
+		
+		while(true)
+		{
+			if((direction == 1 && john < cow && (cow <= (john+disttravel))) || (direction == -1 && john > cow && (cow >= (john-disttravel))))
+			{
+				totaldistance+=Math.abs(cow-john);
+				System.out.println(totaldistance);
+				break;
+			}	
+
+			else
+			{
+				totaldistance+=disttravel*2;
+				disttravel*=2;
+				direction*=-1;
+
+			}
+		}
+	
+		pw.close();
+	}
+}
